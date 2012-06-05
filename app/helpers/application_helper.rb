6dc3a051f5acd64f35deb769fa6aaf2f1904f184
Nicolas Zuasti <zonic@zonic-laptop.(none)>
2012-06-05 17:39:24 -0300

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,10 @@
+module ApplicationHelper
+
+  def app_title
+    'Zonical'
+  end
+
+  def tagline
+    'This is my personal blog'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '73ba0b23ee0303add1336a87d66ff5652f044516301eb6fbb291f091d0420270e2281c532e9d647cbaba8d9e1656a8b7148d4c6bf0e5975b2f0eeadb47a8a96f'
